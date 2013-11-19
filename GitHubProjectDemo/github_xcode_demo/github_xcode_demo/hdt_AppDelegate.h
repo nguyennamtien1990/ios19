aea266e69375ed194a7816afb818650ddfc869cb
nguyennamtien1990 <namtien90nnt@gmail.com>
2013-11-19 19:51:42 +0700

    no message

---
+++ GitHubProjectDemo/github_xcode_demo/github_xcode_demo/hdt_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  hdt_AppDelegate.h
+//  github_xcode_demo
+//
+//  Created by ios19 on 11/19/13.
+//  Copyright (c) 2013 ios19. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface hdt_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitHubProjectDemo/github_xcode_demo/github_xcode_demo/hdt_ViewController.m
@@ -0,0 +1,29 @@
+//
+//  hdt_ViewController.m
+//  github_xcode_demo
+//
+//  Created by ios19 on 11/19/13.
+//  Copyright (c) 2013 ios19. All rights reserved.
+//
+
+#import "hdt_ViewController.h"
+
+@interface hdt_ViewController ()
+
+@end
+
+@implementation hdt_ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
